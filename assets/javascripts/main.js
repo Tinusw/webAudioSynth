c31b07e05a1f851fc3b8af58e56a9646a83d4df5
Tinus Wagner <tinus@3months.com>
2016-09-20 21:06:36 +1200

    wrapped everything in a function

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,43 +1,45 @@
 var context = new AudioContext();
 
-// Create OSC & volume
-var osc1 = context.createOscillator(),
-    osc2 = context.createOscillator();
-var volume = context.createGain();
 
-// Volume scaled to 8/10ths of total gain
-volume.gain.value = 0.8;
+// Play OSC at certain frequency and for a certain time
+var playNote = function(frequency, startTime, duration){
+  // Create OSC & volume
+  var osc1 = context.createOscillator();
+  var osc2 = context.createOscillator();
+  var volume = context.createGain();
 
-// Set Oscillator shapes
-osc1.type = 'sine';
-osc2.type = 'ramp';
+  // Volume scaled to 8/10ths of total gain
+  volume.gain.value = 0.8;
 
-// base frequency
-var frequency = 493.883;
+  // Set Oscillator shapes
+  osc1.type = 'ramp';
+  osc2.type = 'ramp';
 
-// set up nodes so we route osc through volume
-osc1.connect(volume);
-osc2.connect(volume);
+  // set up nodes so we route osc through volume
+  osc1.connect(volume);
+  osc2.connect(volume);
+  volume.connect(context.destination);
 
-// Alter frequency for each OSC independently, chorus effect
-osc1.frequency.value = frequency + 1;
-osc2.frequency.value = frequency - 2;
+  // Alter frequency for each OSC independently, chorus effect
+  osc1.frequency.value = frequency + 1;
+  osc2.frequency.value = frequency - 2;
 
-// Connect Oscillator to our speakers
-volume.connect(context.destination);
+  // Fade out
+  volume.gain.setValueAtTime(0.4, startTime + duration - 0.800);
+  volume.gain.linearRampToValueAtTime(0, startTime + duration);
 
+  // Start the Oscillator now
+  osc1.start(startTime);
+  osc2.start(startTime);
 
-var duration = 2;
-var startTime = context.currentTime;
+  // Stop the Oscillator after 3 sec
+  osc1.stop(startTime + duration);
+  osc2.stop(startTime + duration);
+};
 
-// Fade Out Effect
-volume.gain.setValueAtTime(0.1, startTime + duration - 0.05);
-volume.gain.linearRampToValueAtTime(0, startTime + duration);
+playNote(493.883, context.currentTime, 1.200);
 
-// Start the Oscillator now
-osc1.start(startTime);
-osc2.start(startTime);
+playNote(659.255, context.currentTime + 0.300, 1.200);
 
-// Stop the Oscillator after 3 sec
-osc1.stop(startTime + duration);
-osc2.stop(startTime + duration);
+playNote(987.77, context.currentTime + 0.650, 1.200);
+
