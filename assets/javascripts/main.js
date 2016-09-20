f032b26e5565c080849c41b220b17f0b06ab575c
Tinus Wagner <tinus@3months.com>
2016-09-20 20:53:12 +1200

    dual OSC added, plus chorus effect plus some form of volume control

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,13 +1,39 @@
-var audioContext = new AudioContext();
+var context = new AudioContext();
 
-var context = new AudioContext(),
-    oscillator = context.createOscillator();
+// Create OSC & volume
+var osc1 = context.createOscillator(),
+    osc2 = context.createOscillator();
+var volume = context.createGain();
+
+// Volume scaled to 8/10ths of total gain
+volume.gain.value = 0.8;
+
+// Set Oscillator shapes
+osc1.type = 'sine';
+osc2.type = 'ramp';
+
+// base frequency
+var frequency = 493.883;
+
+// set up nodes so we route osc through volume
+osc1.connect(volume);
+osc2.connect(volume);
+
+// Alter frequency for each OSC independently, chorus effect
+osc1.frequency.value = frequency + 1;
+osc2.frequency.value = frequency - 2;
 
 // Connect Oscillator to our speakers
-oscillator.connect(context.destination);
+volume.connect(context.destination);
+
+
+var duration = 2;
+var startTime = context.currentTime;
 
 // Start the Oscillator now
-oscillator.start(context.currentTime);
+osc1.start(startTime);
+osc2.start(startTime);
 
 // Stop the Oscillator after 3 sec
-oscillator.stop(context.currentTime + 3);
+osc1.stop(startTime + duration);
+osc2.stop(startTime + duration);
