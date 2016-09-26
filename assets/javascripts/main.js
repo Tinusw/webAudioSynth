ca73473835a1f94a791b3b23cdd3d499e15434be
Tinus Wagner <tinus@3months.com>
2016-09-26 13:53:41 +1300

    implimented qwert Hancock

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,137 +1,184 @@
-var context = new AudioContext();
+// Browser Compatibility
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-// Create audio Analyser
-var analyser = context.createAnalyser();
-// Now we need to capture the data
-analyser.fftSize = 2048;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
+  var context = new AudioContext(),
+      settings = {
+          id: 'keyboard',
+          width: 600,
+          height: 150,
+          startNote: 'A2',
+          whiteNotesColour: '#fff',
+          blackNotesColour: '#000',
+          borderColour: '#000',
+          activeColour: 'yellow',
+          octaves: 2
+      },
+      keyboard = new QwertyHancock(settings);
 
-var STATE = {
-  volume: 0.8,
-  melody: function() {}
-}
+  masterGain = context.createGain();
+  nodes = [];
 
-function changeVolume(volume) {
-  STATE.volume = volume
-}
+  masterGain.gain.value = 0.3;
+  masterGain.connect(context.destination); 
 
-function applyVolumeToAmp(amp, startTime, duration) {
-  amp.gain.value = STATE.volume;
-  // Fade out
-  amp.gain.setValueAtTime(STATE.volume, startTime + duration - 0.800);
-  amp.gain.linearRampToValueAtTime(0.0, startTime + duration);  
-}
+  // Keydown Event
+  keyboard.keyDown = function (note, frequency) {
+      var oscillator = context.createOscillator();
+      oscillator.type = 'square';
+      oscillator.frequency.value = frequency;
+      oscillator.connect(masterGain);
+      oscillator.start(0);
 
-function setupVolumeListener(amp, startTime, duration) {
-  //TODO: remove the old listeners before addin this one
-  document.getElementById('volume').addEventListener('change', function() {
-    changeVolume(this.value)
-    applyVolumeToAmp(amp, startTime, duration)
-  })
-}
+      nodes.push(oscillator);
+  };
 
-// Play OSC at certain frequency and for a certain time
-var playNote = function(frequency, startTime, duration){
-  // Create OSC & volume
-  var osc1 = context.createOscillator();
-  var osc2 = context.createOscillator();
-  var amp = context.createGain();
+  // KeyUp or stop note event
+  keyboard.keyUp = function (note, frequency) {
+      var new_nodes = [];
 
-  // default Amp level
-  amp.gain.value = 0.2;
+      for (var i = 0; i < nodes.length; i++) {
+          if (Math.round(nodes[i].frequency.value) === Math.round(frequency)) {
+              nodes[i].stop(0);
+              nodes[i].disconnect();
+          } else {
+              new_nodes.push(nodes[i]);
+          }
+      }
 
-  // Create a basic LP filter
-  var filter = context.createBiquadFilter();
-  filter.type = 0;
-  filter.frequency.value = 500;
-  filter.Q.value = 0;
+      nodes = new_nodes;
+  };
+;
 
-  // Set Oscillator shapes
-  osc1.type = 'sawtooth';
-  osc2.type = 'sawtooth';
 
-  // set up nodes so we route osc through volume
-  osc1.connect(filter);
-  osc2.connect(filter);
 
-  filter.connect(amp);
-  amp.connect(context.destination);
-  // and to analyser
-  amp.connect(analyser)
-  analyser.getByteTimeDomainData(dataArray);
+// var STATE = {
+//   volume: 0.8,
+//   melody: function() {}
+// }
 
-  // Alter frequency for each OSC independently, chorus effect
-  osc1.frequency.value = frequency + 1;
-  osc2.frequency.value = frequency - 2;
+// function changeVolume(volume) {
+//   STATE.volume = volume
+// }
 
-  applyVolumeToAmp(amp, startTime, duration)
-  setupVolumeListener(amp, startTime, duration)
+// function applyVolumeToAmp(amp, startTime, duration) {
+//   amp.gain.value = STATE.volume;
+//   // Fade out
+//   amp.gain.setValueAtTime(STATE.volume, startTime + duration - 0.800);
+//   amp.gain.linearRampToValueAtTime(0.0, startTime + duration);  
+// }
+
+// function setupVolumeListener(amp, startTime, duration) {
+//   //TODO: remove the old listeners before addin this one
+//   document.getElementById('volume').addEventListener('change', function() {
+//     changeVolume(this.value)
+//     applyVolumeToAmp(amp, startTime, duration)
+//   })
+// }
+
+// // Play OSC at certain frequency and for a certain time
+// var playNote = function(frequency, startTime, duration){
+//   // Create OSC & volume
+  
+//   var amp = context.createGain();
+
+//   // default Amp level
+//   amp.gain.value = 0.2;
+
+//   // Create a basic LP filter
+//   var filter = context.createBiquadFilter();
+//   filter.type = 0;
+//   filter.frequency.value = 500;
+//   filter.Q.value = 0;
+
+//   // Set Oscillator shapes
+//   osc1.type = 'sawtooth';
+//   osc2.type = 'sawtooth';
+
+//   // set up nodes so we route osc through volume
+//   osc1.connect(filter);
+//   osc2.connect(filter);
+
+//   filter.connect(amp);
+//   amp.connect(context.destination);
+//   // and to analyser
+//   amp.connect(analyser)
+//   analyser.getByteTimeDomainData(dataArray);
+
+//   // Alter frequency for each OSC independently, chorus effect
+//   osc1.frequency.value = frequency + 1;
+//   osc2.frequency.value = frequency - 2;
+
+//   applyVolumeToAmp(amp, startTime, duration)
+//   setupVolumeListener(amp, startTime, duration)
   
 
-  // Start the Oscillator now
-  osc1.start(startTime);
-  osc2.start(startTime);
+//   // Start the Oscillator now
+//   osc1.start(startTime);
+//   osc2.start(startTime);
 
-  // Stop the Oscillator after 3 sec
-  osc1.stop(startTime + duration);
-  osc2.stop(startTime + duration);
-};
+//   // Stop the Oscillator after 3 sec
+//   osc1.stop(startTime + duration);
+//   osc2.stop(startTime + duration);
+// };
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("click").addEventListener('click', function(){
-    playNote(493.883, context.currentTime, 1.000);
-    playNote(659.255, context.currentTime + 0.300, 1.000);
-    playNote(987.77, context.currentTime + 0.650, 1.000);
-    playNote(880.00, context.currentTime + 1.000, 1.000);
-    playNote(830.61, context.currentTime + 1.500, 1.000);
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//   var osc1 = context.createOscillator();
+//   var osc2 = context.createOscillator();
+//   var amp = context.createGain();
 
-  document.getElementById("A").addEventListener('click', function(){
-    playNote(440, context.currentTime, 1.000);
-  });
+//   var keyboard = new QwertyHancock({
+//                  id: 'keyboard',
+//                  width: 600,
+//                  height: 150,
+//                  octaves: 2,
+//                  startNote: 'A3',
+//                  whiteNotesColour: 'white',
+//                  blackNotesColour: 'black',
+//                  hoverColour: '#f3e939'
+//             });
 
-  document.getElementById("A#").addEventListener('click', function(){
-    playNote(466.16, context.currentTime, 1.000);
-  });
+//   keyboard.keyDown = function (note, frequency) {
+    
 
-  document.getElementById("B").addEventListener('click', function(){
-    playNote(493.88, context.currentTime, 1.000);
-  });
+//     // default Amp level
+//     // amp.gain.value = 0.2;
 
-  document.getElementById("C").addEventListener('click', function(){
-    playNote(523.25, context.currentTime, 1.000);
-  });
+//     // Create a basic LP filter
+//     osc1.connect(amp);
+//     osc2.connect(amp);
 
-  document.getElementById("C#").addEventListener('click', function(){
-    playNote(554.37, context.currentTime, 1.000);
-  });
+//     amp.connect(context.destination)
+//     osc1.start(0);
+//     osc2.start(0);
 
-  document.getElementById("D").addEventListener('click', function(){
-      playNote(587.33, context.currentTime, 1.000);
-    });
+//     nodes.push(oscillator)
+//   };
 
-  document.getElementById("D#").addEventListener('click', function(){
-      playNote(622.25, context.currentTime, 1.000);
-    });
+//   keyboard.keyUp = function (note, frequency) {
+//     var new_nodes = [];
+//     osc1.stop(0);
+//     osc2.stop(0);
 
-  document.getElementById("E").addEventListener('click', function(){
-      playNote(659.25, context.currentTime, 1.000);
-    });
+//     for (var i = 0; i < nodes.length; i++) {
+//         if (Math.round(nodes[i].frequency.value) === Math.round(frequency)) {
+//             nodes[i].stop(0);
+//             nodes[i].disconnect();
+//         } else {
+//             new_nodes.push(nodes[i]);
+//         }
+//     }
 
-  document.getElementById("F").addEventListener('click', function(){
-      playNote(698.46, context.currentTime, 1.000);
-    });
+//     nodes = new_nodes;
+//   };
 
-  document.getElementById("F#").addEventListener('click', function(){
-      playNote(739.99, context.currentTime, 1.000);
-    });
 
-  document.getElementById("G").addEventListener('click', function(){
-      playNote(783.99, context.currentTime, 1.000);
-    });
 
-  document.getElementById("G#").addEventListener('click', function(){
-      playNote(830.61, context.currentTime, 1.000);
-    });
-});
+//   document.getElementById("click").addEventListener('click', function(){
+//     playNote(493.883, context.currentTime, 1.000);
+//     playNote(659.255, context.currentTime + 0.300, 1.000);
+//     playNote(987.77, context.currentTime + 0.650, 1.000);
+//     playNote(880.00, context.currentTime + 1.000, 1.000);
+//     playNote(830.61, context.currentTime + 1.500, 1.000);
+//   });
+
+// });
