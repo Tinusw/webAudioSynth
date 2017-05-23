b6b509854bde4924bcc445233aa9d7b7339299be
Tinus Wagner <tinus@3months.com>
2017-05-23 16:57:33 +1200

    attempting to minify my js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,28 @@
+var webpack = require('webpack');
+
 module.exports = {
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
   entry: "./src/entry.js",
   output: {
     path: __dirname,
